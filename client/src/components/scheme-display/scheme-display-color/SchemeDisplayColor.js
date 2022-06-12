4a293cdd9194585cc28d8a2b8c49968b892296fb
Blaze <blazej.opyrchal@gmail.com>
2022-06-12 23:53:58 +0200

    show color name on hover, adjust media queries

--- client/src/components/scheme-display/scheme-display-color/SchemeDisplayColor.js
+++ client/src/components/scheme-display/scheme-display-color/SchemeDisplayColor.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 function SchemeDisplayColor(props) {
+      const [isHovering, setIsHovering] = useState(false);
+
+      const handleMouseOver = () => {
+            setIsHovering(true);
+      }
+
+      const handleMouseOut = () => {
+            setIsHovering(false);
+      }
 
     return (
-          <div className={[props.color, "scheme-display-color-div"].join(" ")}></div>
+            <div className="scheme-display-color-container">
+          <div className={[props.color, "scheme-display-color-div"].join(" ")} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}></div>
+          {isHovering && <div className="scheme-color-description"><p>{props.color}</p></div>}
+          </div>
     );
   }
 
