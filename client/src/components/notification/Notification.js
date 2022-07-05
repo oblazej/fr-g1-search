11588b34cd2d7751d9fb35425cad76a15f8e8473
Blaze <blazej.opyrchal@gmail.com>
2022-07-05 23:46:29 +0200

    adjust notification bar

--- client/src/components/notification/Notification.js
+++ client/src/components/notification/Notification.js
@@ -1,11 +1,22 @@
+import { useState, forwardRef, useImperativeHandle } from "react";
 import "./Notification.css";
 
-function Notification(props) {
+const Notification = forwardRef((props, ref) => {
+    const [showNotification, setShowNotification] = useState(false);
+
+    useImperativeHandle(ref, () => ({
+        show() {
+            setShowNotification(true);
+            setTimeout(() => {
+                setShowNotification(false);
+            }, 3000)
+        }
+    }));
 
     return (
-        <div className="notification">{props.message}
+        <div className={showNotification ? "notification show" : "notification hide"}>{props.message}
         </div>
     );
-}
+})
 
 export default Notification;
